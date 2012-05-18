5bf5cbd49dce5349623617ecc06b8a1529bc8002
Ramiro Gomez <web@ramiro.org>
2012-05-18 23:19:26 +0200

    map settings

--- commitmap.js
+++ commitmap.js
@@ -1,54 +1,70 @@
-var query = null;
+google.load('visualization', '1', {'packages': ['geochart']});
+
 var dataurl = 'https://docs.google.com/spreadsheet/ccc?key=0Ajfu-hBSP-VLdFlkbFpWYTBxQ3Z4Mjc4bWxtUE5zX1E ';
 
 var settings = {
-    colorschemes: [
-//        ['purple', 'darkred', 'red', 'orange', 'yellow', 'lightgreen', 'green'],
-        ['#F7FCFD', '#E5F5F9', '#CCECE6', '#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#006D2C', '#00441B'], // sequential
-        ['#FFFFCC', '#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'], // sequential
+    colorSchemes: [
         ['#A50026', '#D73027', '#F46D43', '#FDAE61', '#FEE090', '#FFFFBF', '#E0F3F8', '#ABD9E9', '#74ADD1', '#4575B4', '#313695'], // divering red to blue
+        ['#FFFFCC', '#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'], // sequential yellow to red
+        ['#F7FCFD', '#E5F5F9', '#CCECE6', '#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#006D2C', '#00441B'], // sequential blue to green
     ],
-    colorscheme: 2,
-    types: ['relative', 'absolute'],
-    type: 0,
+    colorScheme: 0,
+    dataTypes: ['relative', 'absolute', 'population'],
+    dataType: 0,
     maxValue: {
         'relative': 550,
-        'absolute': 500000
+        'absolute': null,
+        'population': null
     },
-    query: {
-        'relative': 'SELECT A,D ORDER BY D DESC',
-        'absolute': 'SELECT A,B ORDER BY B DESC'
+    queryCol: {
+        'relative': 'D',
+        'absolute': 'B',
+        'population': 'E'
     },
-    regions: {
+    queryStr: 'SELECT A, # ORDER BY # DESC',
+    mapRegions: {
         'World': null,
-        'Africa': '002',
+
         'Americas': '019',
-        'Asia': '142',
+        'Northern America': '021',
+        'Caribbean': '029',
+        'Central America': '013',
+        'South America': '005',
+
         'Europe': '150',
-        'Oceania': '009'
-    },
-    region: 'World' // show world by default
-};
+        'Northern Europe': '154',
+        'Western Europe': '155',
+        'Eastern Europe': 151,
+        'Southern Europe': '039',
 
-var mapOptions = {
-    colorAxis: {
-        colors: settings.colorschemes[settings.colorscheme],
-        maxValue: settings.maxValue[settings.types[settings.type]]
-    },
-    datalessRegionColor: 'FFFFFF',
-    width:900,
-    height:500,
-    region: settings.regions[settings.region]
-};
+        'Asia': '142',
+        'Central Asia': '143',
+        'Eastern Asia': '030',
+        'Southern Asia': '034',
+        'South-Eastern Asia': '035',
+        'Western Asia': '145',
 
-google.load('visualization', '1', {'packages': ['geochart']});
-google.setOnLoadCallback(drawMap);
+        'Africa': '002',
+        'Northern Africa': '015',
+        'Western Africa': '011',
+        'Middle Africa': '017',
+        'Eastern Africa': '014',
+        'Southern Africa': '018',
+
+        'Oceania': '009',
+        'Australia and New Zealand': '053',
+        'Melanesia': '054',
+        'Micronesia': '057',
+        'Polynesia': '061'
+    },
+    mapRegion: null // show world by default
+};
 
 function drawMap() {
-    if (null === query) {
-      query = new google.visualization.Query(dataurl);
-    }
-    query.setQuery(settings.query[settings.types[settings.type]]);
+    query = new google.visualization.Query(dataurl);
+    var queryStr = settings.queryStr.replace(/#/g, settings.queryCol[settings.dataTypes[settings.dataType]]);
+console.log(queryStr);
+    query.setQuery(queryStr);
     query.send(handleQueryResponse);
 }
 
@@ -58,5 +74,35 @@ function handleQueryResponse(response) {
         return;
     }
     var vis = new google.visualization.GeoChart(document.getElementById('map'));
-    vis.draw(response.getDataTable(), mapOptions);
+    var dataTable = response.getDataTable();
+    vis.draw(dataTable, {
+        colorAxis: {
+            colors: settings.colorSchemes[settings.colorScheme],
+            maxValue: settings.maxValue[settings.dataTypes[settings.dataType]]
+        },
+        datalessmapRegionColor: 'FFFFFF',
+        width:900,
+        height:500,
+        region: settings.mapRegions[settings.mapRegion]
+    });
+
+slide(dataTable);
 }
+
+function slide(dataTable) {
+  var col = settings.queryCol[settings.dataTypes[settings.dataType]];
+  var maxVal = dataTable[col][0].c[1].v;
+console.log(maxVal);
+}
+
+$(function(){
+  drawMap();
+  $('.mapSettings').change(function(evt){
+    if ('null' !== this.value && 'selected' !== this.selected) {
+      settings[this.id] = this.value;
+      $('#map').empty();
+      drawMap();
+    };
+  });
+});
+
